854ce4c0bbcd32ea7768c00cd8edd83547a7bb74
YazeedGhazalat <yazeedghazalat@gmail.com>
2022-08-28 12:03:56 +0300

    My 2ed edit

--- main.dart
+++ main.dart
@@ -1,12 +1,21 @@
 void main() {
- List myColors=['red', 'blue', 'green','black', 'yellow','azure', 'orange','acua','brown'];
- for (var i = 0; i < myColors.length; i++) {
-   if(myColors[i][0]=="a"||myColors[i][0]=="b"){
-    print(myColors[i].toString().toUpperCase());
-   }
-   else {
-    print("Sorry This color does not contain a or b on the first character and it contains #${myColors[i].length} of characters");
-   }
-
- } 
+  List myColors = [
+    'red',
+    'blue',
+    'green',
+    'black',
+    'yellow',
+    'azure',
+    'orange',
+    'acua',
+    'brown'
+  ];
+  for (var i = 0; i < myColors.length; i++) {
+    if (myColors[i][0] == "a" || myColors[i][0] == "b") {
+      print(myColors[i].toString().toUpperCase());
+    } else {
+      print(
+          "Sorry This color does not contain a or b on the first character and it contains #${myColors[i].length} of characters");
+    }
+  }
 }
