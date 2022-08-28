9bf50a71755b58e036f0da97fd54e1e3025055fb
YazeedGhazalat <yazeedghazalat@gmail.com>
2022-08-28 13:12:02 +0300

    My 4th edit

--- main.dart
+++ main.dart
@@ -8,14 +8,15 @@ void main() {
     'azure',
     'orange',
     'acua',
-    'brown'
+    'brown',
+    'pink'
   ];
   for (var i = 0; i < myColors.length; i++) {
     if (myColors[i][0] == "a" || myColors[i][0] == "b") {
       print(myColors[i].toString().toUpperCase());
     } else {
       print(
-          "Sorry This color does not start by (a or b)  and it contains #${myColors[i].length}  characters");
+          "Sorry This color (${myColors[i]}) does not start by (a or b)  and it contains #${myColors[i].length}  characters ");
     }
   }
 }
